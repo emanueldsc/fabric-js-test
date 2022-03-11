69176d24f88d6aa384a808233229bec8b9253a33
Emanuel Douglas <emanuel.costa@lds.inf.br>
2022-03-11 01:16:20 -0300

    movendo linhas com parametros dinamicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Call } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { fabric } from 'fabric';
 import { Canvas, Circle, Line } from 'fabric/fabric-impl';
@@ -24,51 +25,62 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.board = new fabric.Canvas('canvas')
+    const line1 = this.makeLine([250, 125, 250, 175])
+    const line2 = this.makeLine([250, 175, 250, 250])
+    const line3 = this.makeLine([250, 250, 300, 350])
+    const line4 = this.makeLine([300, 350, 200, 350])
+    const line5 = this.makeLine([200, 350, 175, 225])
+    const line6 = this.makeLine([175, 225, 325, 225])
 
-    this.board.on('mouse:dblclick', evt => {
-      const left = evt.pointer!.x
-      const top = evt.pointer!.y
-      const pointer = new Pointer(left, top)
-      
-      if(this.path.length == 0) {
-        this.path.push(pointer)
-        this.board!.add(this.makeCircle(pointer))
-      } else {
-        const lastPointer: Pointer = this.path[this.path.length-1] as Pointer
-        const connector: Connector = [lastPointer.left, lastPointer.top, pointer.left, pointer.top]
-        this.path.push(connector)
-        this.path.push(pointer)
+    this.board.add(
+      line1,
+      line2,
+      line3,
+      line4,
+      line5,
+      line6
+    )
 
-        this.board!.add(this.makeLine(connector))
-        this.board!.add(this.makeCircle(pointer))
-      }
+    this.board.add(
+      this.makeCircle(line1.get('x1') as number, line1.get('y1') as number, line1),
+      this.makeCircle(line2.get('x1') as number, line2.get('y1') as number, line1, line2),
+      this.makeCircle(line3.get('x1') as number, line3.get('y1') as number, line2, line3),
+      this.makeCircle(line4.get('x1') as number, line4.get('y1') as number, line3, line4),
+      this.makeCircle(line5.get('x1') as number, line5.get('y1') as number, line4, line5),
+      this.makeCircle(line6.get('x1') as number, line6.get('y1') as number, line5, line6),
+      this.makeCircle(line6.get('x2') as number, line6.get('y2') as number, line6),
+    )
 
-    })
-
-    this.board.on('object:moving', evt => {
-      console.log(evt.target)
-    })
-
+    this.board.on('object:moving', (e) => {
+      const p = e.target as any;
+      const [after, before] = p.lines
+      after && after.set({'x2': p.left, 'y2': p.top})
+      before && before.set({'x1': p.left, 'y1': p.top})
+      this.board?.renderAll()
+    });
   }
 
-  makeCircle(pointer: Pointer): Circle {
-    const circ = new fabric.Circle({
-      left: pointer.left,
-      top: pointer.top,
-      radius: 10,
-      fill: 'transparent',
+  makeCircle(left: number, top: number, ...lines: any[]): Circle {
+    const c: any = new fabric.Circle({
+      left: left,
+      top: top,
+      strokeWidth: 5,
+      radius: 12,
+      fill: '#fff',
       stroke: '#666'
-    })
-    circ.hasControls = false
-    circ.hasBorders = false
-    return circ
+    });
+    c.hasControls = c.hasBorders = false;
+
+    c.lines = lines
+
+    return c;
   }
 
   makeLine(connector: Connector): Line {
     const line = new fabric.Line(connector, {
       fill: 'red',
       stroke: 'red',
-      strokeWidth: 3,
+      strokeWidth: 5,
       selectable: false,
       evented: false
     })
