5e3b3846ca10e49ffb4435d4d79c9b0bf20b501c
Emanuel Douglas <emanuel.costa@lds.inf.br>
2022-03-13 17:38:24 -0300

    Cria pointer ao clicar no canvas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,9 @@ import { Call } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { fabric } from 'fabric';
 import { Canvas, Circle, Line } from 'fabric/fabric-impl';
+import { BehaviorSubject } from 'rxjs';
 
-type Connector = [number, number, number, number]
-
-class Pointer {
-  constructor(
-    public left: number,
-    public top: number,
-  ) { }
-}
+type Pointer = [number, number]
 
 
 @Component({
@@ -20,67 +14,56 @@ class Pointer {
 })
 export class AppComponent implements OnInit {
 
-  path: (Pointer | Connector)[] = []
-  board: Canvas | undefined
+  pointers: any[] = []
+  canvas: Canvas | null = null
+
+  itemsDrawn: BehaviorSubject<(Circle | Line)[]> = new BehaviorSubject<(Circle | Line)[]>([])
 
   ngOnInit(): void {
-    this.board = new fabric.Canvas('canvas')
-    const line1 = this.makeLine([250, 125, 250, 175])
-    const line2 = this.makeLine([250, 175, 250, 250])
-    const line3 = this.makeLine([250, 250, 300, 350])
-    const line4 = this.makeLine([300, 350, 200, 350])
-    const line5 = this.makeLine([200, 350, 175, 225])
-    const line6 = this.makeLine([175, 225, 325, 225])
+    this.canvas = new fabric.Canvas('canvas')
 
-    this.board.add(
-      line1,
-      line2,
-      line3,
-      line4,
-      line5,
-      line6
-    )
+    this.canvas.on('mouse:dblclick', evt => {
+      const left = evt.pointer!.x
+      const top = evt.pointer!.y
 
-    this.board.add(
-      this.makeCircle(line1.get('x1') as number, line1.get('y1') as number, line1),
-      this.makeCircle(line2.get('x1') as number, line2.get('y1') as number, line1, line2),
-      this.makeCircle(line3.get('x1') as number, line3.get('y1') as number, line2, line3),
-      this.makeCircle(line4.get('x1') as number, line4.get('y1') as number, line3, line4),
-      this.makeCircle(line5.get('x1') as number, line5.get('y1') as number, line4, line5),
-      this.makeCircle(line6.get('x1') as number, line6.get('y1') as number, line5, line6),
-      this.makeCircle(line6.get('x2') as number, line6.get('y2') as number, line6),
-    )
+      const circle = this.makeCircle(left, top)
 
-    this.board.on('object:moving', (e) => {
-      const p = e.target as any;
-      const [after, before] = p.lines
-      after && after.set({'x2': p.left+5, 'y2': p.top+5})
-      before && before.set({'x1': p.left+5, 'y1': p.top+5})
-      this.board?.renderAll()
-    });
+      this.pointers.push(circle)
+
+      this.canvas?.add(circle)
+
+    })
+
+    
+    // this.board.on('object:moving', (e) => {
+    //   const p = e.target as any;
+    //   const [after, before] = p.lines
+    //   before && before.set({ 'x1': p.left + 5, 'y1': p.top + 5 })
+    //   after && after.set({ 'x2': p.left + 5, 'y2': p.top + 5 })
+    //   this.board?.renderAll()
+    // });
   }
 
-  makeCircle(left: number, top: number, ...lines: any[]): Circle {
+  
+
+  makeCircle(left: number, top: number): Circle {
     const c: any = new fabric.Circle({
-      left: left -5,
-      top: top -5,
-      strokeWidth: 5,
+      left: left - 3,
+      top: top - 3,
+      strokeWidth: 3,
       radius: 5,
-      fill: '#fff',
-      stroke: '#666'
+      fill: 'red',
+      stroke: 'red'
     });
     c.hasControls = c.hasBorders = false;
-
-    c.lines = lines
-
     return c;
   }
 
-  makeLine(connector: Connector): Line {
-    const line = new fabric.Line(connector, {
+  makeLine(init: Pointer, end: Pointer): Line {
+    const line = new fabric.Line([...init, ...end], {
       fill: 'red',
       stroke: 'red',
-      strokeWidth: 5,
+      strokeWidth: 3,
       selectable: false,
       evented: false
     })
